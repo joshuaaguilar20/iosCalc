dfc01bc14d7fe4d829b4882809a0429d7f78963d
Joshua-Aguilar <joshuaaguilar20@gmail.com>
2018-07-21 20:45:31 -0700

    webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const path = require('path'); //access module by name
 
 module.exports = {
     entry:'./src/app.js',
-    mode:'development',
+    mode:'production',
     output: {
         path: path.join(__dirname,'public'),
         filename:'bundle.js'
