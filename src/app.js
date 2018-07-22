f146da177bf6946f60a1123a0ed2368296924b2c
Joshua-Aguilar <joshuaaguilar20@gmail.com>
2018-07-21 20:39:11 -0700

    removed header

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'normalize.css/normalize.css'
@@ -6,155 +5,96 @@ import './styles/styles.scss'
 
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.clearDisplay = this.clearDisplay.bind(this)
-    this.calculate = this.calculate.bind(this)
-    //this.equalFunction = this.equalFunction.bind(this)
-    this.handleNumber = this.handleNumber.bind(this);
-    this.operatorFunction = this.operatorFunction.bind(this);
-    this.displayFunction = this.displayFunction.bind(this);
-    this.acc = '';
-    this.currentNumber = '';
-    this.flagNewNumber = false;
-    this.operator = '';
-    this.result = '';
-  }
-  state = {
-    calculated:'',
-    display:'0'
-  };
-  
-  operatorFunction = (operator) => {  //This Function Takes string from onClick 'sub'/add and passes the symbol to this.operator then is passed to calculate
-    
-    if(this.flagNewNumber && this.operator === ''){
-      this.displayFunction('Error Invalid Op');
-    }
-    this.flagNewNumber = true;
-     if(this.currentNumber !== '' && this.acc !== ''){
-      this.calculate()
-    }
-    this.operator = operator;
-
-
-
-
-    // if(this.flagNewNumber) {
-    //   debugger;
-    //   this.displayFunction('Error Invalid Op');
-    // } else {
-    //   if(this.operator !== '') {
-    //     this.calculate();
-    //   } 
-    //   this.operator = operator;
-     
-    // }
-    //  this.flagNewNumber = true
-     
-     console.log(` flag ${this.flagNewNumber}`);
-     console.log(` current ${this.currentNumber}`);
-     console.log(` acc ${this.acc}`);
-     console.log(`operator  ${this.operator}`);
-     console.log("this is the OP")
-     
-  }
-  
-  
-  clearDisplay = () => {
-    this.displayFunction('0');
-    this.acc = '';
-    this.currentNumber = '';
-    this.flagNewNumber = false;
-    this.operator = '';
-    
-  }
-  
-  
-  calculate = () =>{
-  ///make new equal function - operator handle- ready for operation to call equals. Calculate function value in this.this.this.this.this.this.acculator perform the operation in operator variable on current 
-  //value 
-    let op1 = parseFloat(this.acc);
-    let op2 = parseFloat(this.currentNumber);
-    switch(this.operator) {
-      case 'add':
-        this.result = op1 + op2;
-        this.displayFunction(this.result);
-        break;
-      case 'sub':
-        this.result = op1 - op2;
-        this.displayFunction(this.result);
-        break;
-      case 'multi':
-        this.result = Math.round(10000 * op1 * op2) / 10000
-        this.displayFunction(this.result);
-        break;
-      case 'div':
-        this.result = Math.round(10000 * op1 / op2) /10000
-        this.displayFunction(this.result);
-        break;
-      default:
-        alert("Invalid Op '" + this.operator + "'")
-    }
-    this.acc = '';
-      this.currentNumber = this.result
-
-      console.log(` flag ${this.flagNewNumber}`);
-      console.log(` current ${this.currentNumber}`);
-      console.log(` acc ${this.acc}`);
-      console.log(`operator  ${this.operator}`);
-      console.log("this is the cal")
-    
-   // Clear Operator and set Current Number to result
-   
-  }
-  // equalFunction = () => {
-  //   debugger;
-  //   this.flagNewNumber = true
-  //   this.calculate();
-  // }
-
-  handleNumber = (number) =>{
-    if(this.flagNewNumber) {
-      this.acc = this.currentNumber
-      this.currentNumber = '' +  number;
+    constructor(props) {
+      super(props);
+      this.acc = '';
+      this.currentNumber = '';
       this.flagNewNumber = false;
-      console.log(` flag ${this.flagNewNumber}`);
-      console.log(` current ${this.currentNumber}`);
-      console.log(` acc ${this.acc}`);
-      console.log(`operator  ${this.operator}`);
-      console.log("this is the # acc handle")                                                             //if . then true
-                                                                                                          // if no decimal then false
-    } else { 
-      if(number !== 0 && number !== '.' || (number === 0 && this.state.display.charAt(0) !== '0' && this.currentNumber.indexOf('.') === -1)
-        || (number === 0 && this.currentNumber.indexOf('.') > -1) 
-        || (number === '.' && this.currentNumber.indexOf('.') === -1)
-      
-      )
-        this.currentNumber += number;
+      this.operator = '';
+      this.result = '';
     }
-    console.log(this.currentNumber);
-    this.displayFunction(this.currentNumber); 
-    console.log(` flag ${this.flagNewNumber}`);
-      console.log(` current ${this.currentNumber}`);
-      console.log(` acc ${this.acc}`);
-      console.log(`operator  ${this.operator}`);
-      console.log("this is the CN handle")
-  
-  }
-  
-  
-  displayFunction = (input) => {
-    this.setState(()=>({display:this.state.display = input}));
-  };
+    state = {
+      display: '0'
+    };
 
+    operatorFunction = (operator) => { //This Function Takes string from onClick 'sub'/add and passes the symbol to this.operator then is passed to calculate
+      if (this.flagNewNumber && this.operator === '') {
+        this.displayFunction('Error Invalid Op');   //this logic prevents erros if a number is pressed incorrectly.
+      }
+      this.flagNewNumber = true;      //resets the flag to true to all for system to store secound number 
+      if (this.currentNumber !== '' && this.acc !== '') {
+        this.calculate()          //logic that tells it when user enters number + number to only calculate if it has two correct values. 
+      }
+      this.operator = operator;   //sets operator to be used by calculate 
+    }
+
+
+    clearDisplay = () => {    //clears the display 
+      this.displayFunction('0');
+      this.acc = '';
+      this.currentNumber = '';
+      this.flagNewNumber = false;
+      this.operator = '';
+
+    }
+    calculate = () => {     //all this does is take string from button below passed to op and do the op based on the string. simple 
+      let op1 = parseFloat(this.acc);
+      let op2 = parseFloat(this.currentNumber);
+      switch (this.operator) {
+        case 'add':
+          this.result = op1 + op2;
+          this.displayFunction(this.result);
+          break;
+        case 'sub':
+          this.result = op1 - op2;
+          this.displayFunction(this.result);
+          break;
+        case 'multi':
+          this.result = Math.round(10000 * op1 * op2) / 10000
+          this.displayFunction(this.result);
+          break;
+        case 'div':
+          this.result = Math.round(10000 * op1 / op2) / 10000
+          this.displayFunction(this.result);
+          break;
+        case 'mod':
+          this.result = op1 % op2
+          this.displayFunction(this.result);
+          break;
+        default:
+          alert("Invalid Op '" + this.operator + "'")
+      }
+      this.acc = '';                //clears the second number 
+      this.currentNumber = this.result    // ***important keeps running total of calcuations
+
+    }                                 // sets numbers as string so it can support numbers like 99 900 etc. 
+    handleNumber = (number) => {    // if flag is true sets second number or ACC
+      if (this.flagNewNumber) {       // if flag is false sets first Number as Current Number 
+        this.acc = this.currentNumber
+        this.currentNumber = '' + number;
+        this.flagNewNumber = false;
+      } else {
+        if (number !== 0 && number !== '.' || (number === 0 && this.state.display.charAt(0) !== '0' && this.currentNumber.indexOf('.') === -1) ||
+          (number === 0 && this.currentNumber.indexOf('.') > -1) ||
+          (number === '.' && this.currentNumber.indexOf('.') === -1))   //All this logic does is say if its 0 dont allow double zero unless                                       
+          this.currentNumber += number;                    //there is a '.' then allow a zero ohh yeah and only allow one . sorry for long logic
+      }
+      this.displayFunction(this.currentNumber);
+    }
+    displayFunction = (input) => {
+      this.setState(() => ({
+        display: this.state.display = input
+      }));
+    };
+                                //below button click fires function and sends number or operator to functions above* 
 render(){
   return (
-    <div>
+    <div>                                                                          
   <div className="container">
   <div className="item item-header" id="display">{this.state.display}</div>
   <button className="btn silver" id="clear" onClick={this.clearDisplay}>C</button>
-  <button className="btn silver">+/-</button>
-  <button className="btn silver">%</button>
+  <button className="btn silver" onClick={() => this.operatorFunction('add')}>+</button>
+  <button className="btn silver" onClick={() => this.operatorFunction('mod')} >%</button>
   <button className="btn orange" id="divide" onClick={() => this.operatorFunction('div')}>÷</button>
   
   <button className="btn" id="seven" onClick={() => this.handleNumber(7)}>7</button>
@@ -173,7 +113,7 @@ render(){
   <button className="btn orange" id="add" onClick={() => this.operatorFunction('add')}>+</button>
   
   <button className="btn" id="zero" onClick={() => this.handleNumber(0)}>0</button>
-  <button className="btn">11</button>
+  <button className="btn" id="clear" onClick={this.clearDisplay}>C</button>
   <button className="btn" id="decimal" onClick={() => this.handleNumber('.')}>.</button>
   <button className="btn orange" id="equals" onClick={() => this.calculate()}>=</button>
   
